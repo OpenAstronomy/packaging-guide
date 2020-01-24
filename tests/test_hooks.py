961a927937798ba741ddb276ec519051cf500013
Stuart Mumford <stuart@cadair.com>
2020-01-24 16:37:22 +0000

    Add tests!

---
+++ tests/test_hooks.py
@@ -0,0 +1,81 @@
+from pathlib import Path
+
+import pytest
+
+
+def test_examples_removed(cookiejar_no_examples):
+    cj = cookiejar_no_examples
+    ctx = cj.context
+
+    example_files = [
+        "example_mod.py",
+        "example_c.pyx",
+        "example_subpkg/"
+        "tests/"
+        ]
+
+    for afile in example_files:
+        assert not cj.project.join(ctx['package_name'], afile).exists()
+
+
+def test_examples_present(cookiejar_examples):
+    cj = cookiejar_examples
+    ctx = cj.context
+
+    example_files = [
+        "example_mod.py",
+        "example_subpkg/",
+        "example_subpkg/__init__.py",
+        "example_subpkg/tests/__init__.py",
+        "example_subpkg/data/.gitignore",
+        "tests/",
+        "tests/test_example.py",
+        "tests/__init__.py",
+        ]
+
+    if ctx['use_compiled_extensions'] == 'y':
+        example_files.append("example_c.pyx")
+
+    for afile in example_files:
+        assert cj.project.join(ctx['package_name'], afile).exists()
+
+
+@pytest.mark.parametrize("license, lfile", [
+    ("BSD 3-Clause", "BSD3.rst"),
+    ("GNU GPL v3+", "GPLv3.rst"),
+    ("Apache Software License 2.0", "APACHE2.rst"),
+    ("BSD 2-Clause", "BSD2.rst")])
+def test_licence(license, lfile, cookies):
+    cj = cookies.bake(extra_context={'license': license})
+
+    assert cj.project.join("licenses", "TEMPLATE_LICENSE.rst").exists()
+    assert cj.project.join("licenses", "LICENSE.rst").exists()
+
+    with open(cj.project.join("licenses", "LICENSE.rst")) as fobj:
+        license_content = fobj.readlines()
+
+    base_path = Path(".") / "{{ cookiecutter.package_name }}"
+    base_path /= "licenses"
+    base_path = base_path.resolve()
+    with open(base_path / lfile) as fobj:
+        expected_content = fobj.readlines()
+
+    assert expected_content[1:] == license_content[1:]
+
+
+def test_other_licence(cookies):
+    cj = cookies.bake(extra_context={'license': 'Other'})
+
+    assert cj.project.join("licenses", "TEMPLATE_LICENSE.rst").exists()
+
+    assert not cj.project.join("licenses", "LICENSE.rst").exists()
+
+
+    license_files = {"BSD 3-Clause": 'BSD3.rst',
+                     "GNU GPL v3+": 'GPLv3.rst',
+                     "Apache Software License 2.0": 'APACHE2.rst',
+                     "BSD 2-Clause": 'BSD2.rst'}
+
+    for name, lfile in license_files.items():
+        assert cj.project.join("licenses", lfile).exists()
+
---
+++ tests/test_rendered_tox.py
@@ -0,0 +1,20 @@
+"""
+Test that tox runs inside a rendered template.
+"""
+
+import subprocess
+
+import tox
+import pytest
+
+
+def test_tox_runs(cookiejar, cmd):
+    """
+    Test that tox runs for all the configs.
+    """
+    # We have to git init so that setuptools_scm works
+    subprocess.call(["git", "init", str(cookiejar.project)])
+
+    # The cmd fixture is imported from tox._pytestplugins and runs tox with the
+    # given CLI args.
+    cmd("-c", str(cookiejar.project))
