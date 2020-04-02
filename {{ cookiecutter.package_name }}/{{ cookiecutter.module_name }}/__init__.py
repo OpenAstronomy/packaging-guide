3c7b828fd81ef9fb38b7ce38ae931e4c113c887e
Stuart Mumford <stuart@cadair.com>
2020-04-02 15:18:36 +0100

    Don't fail if version.py missing

--- {{ cookiecutter.package_name }}/{{ cookiecutter.module_name }}/__init__.py
+++ {{ cookiecutter.package_name }}/{{ cookiecutter.module_name }}/__init__.py
@@ -1,6 +1,9 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
-from .version import __version__
+try:
+    from .version import __version__
+except ImportError:
+    __version__ = "unknown"
 
 {%- if cookiecutter.include_example_code == 'y' %}
 from .example_mod import do_primes
