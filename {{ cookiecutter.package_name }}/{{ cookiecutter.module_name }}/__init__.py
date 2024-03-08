ff5c8642b307e818d4c9ac99fd0c7b21e39c4bd2
Samuel Bennett <CyclingNinja@users.noreply.github.com>
2024-03-08 13:28:22 +0000

    Upstreaming precommit lessons from sunpy-template (#63)
    * Upstreaming precommit lessons from sunpy-template
    
    * Adds py12 tox test suite
    
    * Adds py12 enviroment to parent tox
    
    * Update virtualenv handling in test to pytest-venv

--- {{ cookiecutter.package_name }}/{{ cookiecutter.module_name }}/__init__.py
+++ {{ cookiecutter.package_name }}/{{ cookiecutter.module_name }}/__init__.py
@@ -1,9 +1,11 @@
+{%- if cookiecutter.include_example_code == 'y' -%}
+from .example_mod import do_primes
+{%- endif %}
 from .version import version as __version__
 
-{%- if cookiecutter.include_example_code == 'y' %}
-from .example_mod import do_primes
+{% if cookiecutter.include_example_code == 'y' -%}
 # Then you can be explicit to control what ends up in the namespace,
 __all__ = ['do_primes']
-{% else %}
+{% else -%}
 __all__ = []
-{%- endif -%}
+{% endif -%}
