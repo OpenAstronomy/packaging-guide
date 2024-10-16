8334c24250a0f7da1ce0cb4d0f13ae5b1cc81a7a
CyclingNinja <bennett.sm89@gmail.com>
2024-10-16 14:55:16 +0100

    Adds graphviz arguments to docs/conf

--- {{ cookiecutter.package_name }}/docs/conf.py
+++ {{ cookiecutter.package_name }}/docs/conf.py
@@ -76,6 +76,18 @@ intersphinx_mapping = {"python": ("https://docs.python.org/", None)}
 # a list of builtin themes.
 html_theme = "{{ cookiecutter._sphinx_theme }}"
 
+# Render inheritance diagrams in SVG
+graphviz_output_format = "svg"
+
+graphviz_dot_args = [
+    "-Nfontsize=10",
+    "-Nfontname=Helvetica Neue, Helvetica, Arial, sans-serif",
+    "-Efontsize=10",
+    "-Efontname=Helvetica Neue, Helvetica, Arial, sans-serif",
+    "-Gfontsize=10",
+    "-Gfontname=Helvetica Neue, Helvetica, Arial, sans-serif",
+]
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
