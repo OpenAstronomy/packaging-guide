aec53b81aed2e7e534045e59303d82712fe82fb1
Stuart Mumford <stuart@cadair.com>
2024-10-21 08:34:51 +0100

    Update conf.py

--- {{ cookiecutter.package_name }}/docs/conf.py
+++ {{ cookiecutter.package_name }}/docs/conf.py
@@ -91,7 +91,7 @@ graphviz_dot_args = [
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ["_static"]
+# html_static_path = ["_static"]  # NOQA: ERA001
 
 # By default, when rendering docstrings for classes, sphinx.ext.autodoc will
 # make docs with the class-level docstring and the class-method docstrings,
