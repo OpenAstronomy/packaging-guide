d1437ea8a23479aca7214a49cc5a299c65fb36aa
Stuart Mumford <stuart@cadair.com>
2025-07-07 08:59:50 +0100

    PLC0415

--- {{ cookiecutter.package_name }}/{{ cookiecutter.module_name }}/tests/test_example.py
+++ {{ cookiecutter.package_name }}/{{ cookiecutter.module_name }}/tests/test_example.py
@@ -1,9 +1,9 @@
 {% if cookiecutter.use_compiled_extensions == 'y' %}
 def test_primes_c():
-    from {{ cookiecutter.module_name }}.example_c import primes as primes_c
+    from {{ cookiecutter.module_name }}.example_c import primes as primes_c  # noqa: PLC0415
     assert primes_c(10) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
 {% endif %}
 
 def test_primes():
-    from {{ cookiecutter.module_name }}.example_mod import primes
+    from {{ cookiecutter.module_name }}.example_mod import primes  # noqa: PLC0415
     assert primes(10) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
